44e26bba08afe56dd4eba56e75c6f62619c77481
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2022-04-17 02:01:36 -0400

    if

--- genFlowpics.sh
+++ genFlowpics.sh
@@ -15,10 +15,12 @@ for inp_file in $MY_DIR/cs638pcaps/PcapVoip/*.pcap; do
     python3  $MY_DIR/FlowPic/traffic_csv_converter.py -f $MY_DIR/FlowPic/csvs/$op_csv
     echo "Generated flowpics for $traffic_type"
     echo " "
-    if [[ $inp_file != *"vpn"* ]]; then
-        python3 $MY_DIR/FlowPic/tput.py -f $MY_DIR/FlowPic/csvs/$op_csv
-        echo " "
-        echo "Generated Throughput data for $inp_file"
-        echo " "
+    if [[ $inp_file == *"zoom"* ]] || [[ $inp_file == *"teams"* ]] || [[ $inp_file == *"webex"* ]] || [[ $inp_file == *"google"* ]]; then
+        if [[ $inp_file != *"vpn"* ]]; then
+            python3 $MY_DIR/FlowPic/tput.py -f $MY_DIR/FlowPic/csvs/$op_csv
+            echo " "
+            echo "Generated Throughput data for $inp_file"
+            echo " "
+        fi
     fi
 done
